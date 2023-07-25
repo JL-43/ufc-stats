9847cd922c512775cbca1efebcd087ee17c8ce08
JL <joseluisreyes43@gmail.com>
2023-07-25 14:16:42 +0800

    migrated to using pyspark and delta lake, bronze py script has been adjusted to write delta

--- src/bronze_scrape_ufc_fighters.py
+++ src/bronze_scrape_ufc_fighters.py
@@ -1,10 +1,26 @@
-# 1_scrape_ufc_fighters.py
-import pandas as pd
-from pandas import DataFrame
+## 1_scrape_ufc_fighters.py
+# import pandas as pd
+# from pandas import DataFrame
+
+import pyspark.pandas as ps
+from pyspark.pandas import DataFrame
+import pyspark
+from delta import configure_spark_with_delta_pip
+
+builder = (
+    pyspark.sql.SparkSession.builder.appName("bronze_scrape_ufc_fighters")
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+    .config(
+        "spark.sql.catalog.spark_catalog",
+        "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+    )
+)
+
+spark = configure_spark_with_delta_pip(builder).getOrCreate()
 
 
 def scrape_fighter_data(url: str) -> DataFrame:
-    df = pd.read_html(url)[0]
+    df = ps.read_html(url)[0]
     df.reset_index(drop=True, inplace=True)
 
     return df
@@ -13,4 +29,12 @@ def scrape_fighter_data(url: str) -> DataFrame:
 if __name__ == "__main__":
     ufc_stats_url: str = "http://ufcstats.com/statistics/fighters?char=*&page=all"
     bronze_df: DataFrame = scrape_fighter_data(ufc_stats_url)
-    bronze_df.to_csv("../output/2_ufc_fighters_cleaned.csv", index=False)
+    bronze_df.to_delta("../output/1_ufc_fighters_scraped", index=False)
+
+    read_df: DataFrame = ps.read_delta("../output/1_ufc_fighters_scraped")
+
+    # print(read_df.head())
+
+
+# data = spark.range(0, 5)
+# data.write.format("delta").save("./tmp/delta-table")
