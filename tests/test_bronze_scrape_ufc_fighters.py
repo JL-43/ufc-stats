b9e1647e9feffbd6941a6ea39cd78b6cd69134f1
JL <joseluisreyes43@gmail.com>
2023-07-25 13:23:09 +0800

    added typing hints to tests

--- tests/test_bronze_scrape_ufc_fighters.py
+++ tests/test_bronze_scrape_ufc_fighters.py
@@ -1,10 +1,11 @@
 # test_bronze_scrape_ufc_fighters.py
 import pandas as pd
+from pandas import DataFrame
 from src import bronze_scrape_ufc_fighters
 
 
 def test_scrape_fighter_data():
-    url = "http://ufcstats.com/statistics/fighters?char=a&page=all"
-    df = bronze_scrape_ufc_fighters.scrape_fighter_data(url)
+    url: str = "http://ufcstats.com/statistics/fighters?char=a&page=all"
+    df: DataFrame = bronze_scrape_ufc_fighters.scrape_fighter_data(url)
     assert isinstance(df, pd.DataFrame)
     assert len(df) > 0
--- tests/test_silver_clean_ufc_data.py
+++ tests/test_silver_clean_ufc_data.py
@@ -6,9 +6,9 @@ from src import silver_clean_ufc_data as clean_data
 
 def test_convert_height_in_feet_string_to_cm_double():
     # Test a normal case
-    height_in_feet = "6' 2\""
-    expected_height_in_cm = 187.96
-    actual_height_in_cm = clean_data.convert_height_in_feet_string_to_cm_double(
+    height_in_feet: str = "6' 2\""
+    expected_height_in_cm: float = 187.96
+    actual_height_in_cm: float = clean_data.convert_height_in_feet_string_to_cm_double(
         height_in_feet
     )
     assert actual_height_in_cm == pytest.approx(
@@ -16,9 +16,9 @@ def test_convert_height_in_feet_string_to_cm_double():
     ), "Conversion of height in feet to cm failed for normal case"
 
     # Test a corner case
-    height_in_feet = "0"
-    expected_height_in_cm = 0.0
-    actual_height_in_cm = clean_data.convert_height_in_feet_string_to_cm_double(
+    height_in_feet: str = "0"
+    expected_height_in_cm: float = 0.0
+    actual_height_in_cm: float = clean_data.convert_height_in_feet_string_to_cm_double(
         height_in_feet
     )
     assert actual_height_in_cm == pytest.approx(
